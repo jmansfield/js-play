b5ef7c89023dd17d2cfbde0f68b0299c0ed3452e
James Mansfield <james.mansfield@gmail.com>
2016-02-03 21:43:42 +1100

    Simple element finding on page load

---
+++ 01 Finding elements/application.js
@@ -0,0 +1,6 @@
+// This event handler waits for the page to load before executing the code inside to be run
+$(document).ready(function() {
+  $("h1"); //Selects all H1's
+  $("h1").text(); //Selects all H1's and stores the text of them in variables
+  $("h1").text("Change the text to this"); //Selects all H1's and changes the text of them to what's in the string
+});
