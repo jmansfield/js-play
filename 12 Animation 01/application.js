181c2392890740de6323bcfd2d3756a3e571a7fa
James Mansfield <james.mansfield@gmail.com>
2016-02-12 01:35:20 +1100

    Basic animation

--- 12 Animation 01/application.js
+++ 12 Animation 01/application.js
@@ -1,16 +1,15 @@
 // This event handler waits for the page to load before executing the code inside to be run
 $(document).ready(function() {
-  $('#all-tours').on('click', '.tour', function() {
-    // alert("Hello world");
-    $(this).toggleClass('highlighted');
+  $('#all-tours').on('click', '.tour', function() { // On clicking an element with class tour within an element with id all-tours
+    $(this).toggleClass('highlighted'); // Add and remove (toggle) the class highlighted
 
     // Check to see if the current element has a class of highlighted and if so animate it up
     if ($(this).hasClass('highlighted')) {
       $(this).find('.comments').show();
-      // $(this).animate({'top': '-50px'});
+      // $(this).animate({'top': '-50px'}); This would animate it but we've done it with CSS
     } else { // Else if it doesn't animate it closed
       $(this).find('.comments').hide();
-      // $(this).animate({'top': '0px'});
+      // $(this).animate({'top': '0px'}); This would animate it but we've done it with CSS
     }
   });
 });
