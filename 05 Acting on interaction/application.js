d82d0be6486ec255253a99de2db7fc4b24588a33
James Mansfield <james.mansfield@gmail.com>
2016-02-12 01:00:45 +1100

    Added specificity to replacements

--- 05 Acting on interaction/application.js
+++ 05 Acting on interaction/application.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   // This event handler waits for a click on a button element before running //
   $('button').on('click', function(){
     var price = $('<p>From $399</p>'); //Set a variable that is html code
-    $('.vacation').append(price);
-    $('button').remove(); // Removes the button
+    $(this).closest('.vacation').append(price); // Traverses up the DOM to find the closest ancestor with a vacation class and then appends
+    $(this).remove(); // Removes the button
   });
 });
